55aad2146c6233d143b58b2f2dca96a8405d76e9
vinocos <vinocos@gmail.com>
2014-07-17 21:55:59 -0400

    Formatting code
    Make hex decoding and encoding consistent
    
    Add Authentication: If message, check signature with message; else
    check signature with user's public key
    
    Add adduser and modifyuser: API structure slightly modified
    
    Create client.py

--- client/key.py
+++ client/key.py
@@ -1,20 +1,25 @@
 from pyelliptic.ecc import ECC
+from binascii import hexlify, unhexlify
 import json
 
-def parsekey(key):
-    return ''.join(['%02x' % i for i in key ]).upper()
-    
-def generate_keypair(filename = 'account'):
-	key = ECC()
-	keypair = {'public_key' : parsekey(key.get_pubkey()),
-                   'private_key' : parsekey(key.get_privkey())
-                   }
-	with open('keys/' + filename + '.key', 'w') as f:
-		f.write(json.dumps(keypair))
 
-def load_keypair(filename = 'account'):
+def parsekey(key):
+    return hexlify(key).decode().upper()
+
+
+def generate_keypair(filename='account'):
+    key = ECC()
+    keypair = {'public_key': parsekey(key.get_pubkey()),
+               'private_key': parsekey(key.get_privkey())
+               }
+    with open('keys/' + filename + '.key', 'w') as f:
+        f.write(json.dumps(keypair))
+
+
+def load_ECC_and_keypair(filename='account'):
     with open('keys/' + filename + '.key') as f:
         keypair = json.loads(f.read())
-    return ECC(pubkey = bytes.fromhex(keypair["public_key"]),
-               privkey = bytes.fromhex(keypair["private_key"]))
+    return ECC(pubkey=unhexlify(keypair["public_key"]),
+               privkey=unhexlify(keypair["private_key"])), keypair
+
 
--- client/postrec.py
+++ client/postrec.py
@@ -1,32 +1,54 @@
 import json
-import urllib.request, urllib.parse
+import urllib.request
+import urllib.parse
+
 
 def GetRequest(des, postdata):
-	params = urllib.parse.urlencode(postdata).encode('utf-8')
-	header = {'User-Agent':'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)'}
-	req = urllib.request.Request(
-		url = des,
-		data = params,
-		headers = header
-		)
-	return urllib.request.urlopen(req).read().decode("UTF8")
+    params = urllib.parse.urlencode(postdata).encode('utf-8')
+    header = {
+        'User-Agent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)'}
+    req = urllib.request.Request(
+        url=des,
+        data=params,
+        headers=header
+    )
+    return urllib.request.urlopen(req).read().decode("UTF8")
+
 
 def parsemsg(req):
-        for i in map(json.loads, json.loads(req)):
-                print(i)
+    for i in map(json.loads, json.loads(req)):
+        print(i)
 
-def testreceive(des, key):
-        postdata = {'action' : 'receive', des : key}
-        request = GetRequest("http://msg.raycursive.com/api.php", postdata)
-        parsemsg(request)
 
-def testsend(keyfrom, keyto, message):
-        postdata = {'action' : 'send', 'from' : keyfrom, 'to' : keyto, 'message': message}
-        request = GetRequest("http://msg.raycursive.com/api.php", postdata)
-        print(request)
+def testreceive(des, key, signature):
+    postdata = {'action': 'receive', des: key, 'signature': signature}
+    request = GetRequest("http://msg.raycursive.com/api.php", postdata)
+    parsemsg(request)
+
+
+def testsend(keyfrom, keyto, signature, message):
+    postdata = {'action': 'send', 'from': keyfrom,
+                'to': keyto, 'signature': signature, 'message': message}
+    request = GetRequest("http://msg.raycursive.com/api.php", postdata)
+    print(request)
+
 
 def testdelete(key, des, signature, message):
-        postdata = {'action' : 'delete', des : key, 'signature' : signature, 'message': message}
-        request = GetRequest("http://msg.raycursive.com/api.php", postdata)
-        print(postdata)
-        print(request)
+    postdata = {'action': 'delete', des: key,
+                'signature': signature, 'message': message}
+    request = GetRequest("http://msg.raycursive.com/api.php", postdata)
+    print(postdata)
+    print(request)
+
+
+def adduser(key, username=key, email='', signature):
+    postdata = {'action': 'adduser', 'key': key,
+                'email': email, 'signature': signature}
+    request = GetRequest("http://msg.raycursive.com/api.php", postdata)
+    print(request)
+
+def modifyuser(key, username=key, email='', signature):
+    postdata = {'action': 'modifyuser', 'key': key,
+                'email': email, 'signature': signature}
+    request = GetRequest("http://msg.raycursive.com/api.php", postdata)
+    print(request)
